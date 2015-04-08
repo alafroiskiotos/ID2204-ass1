a3a44b10c6a7ef030c0503640296d5dd68887fc5
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-04-08 16:28:09 +0200

    Remove debugging messages

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,15 +1,10 @@
-/*
- * sudoku.cpp
- *
- *  Created on: Apr 8, 2015
- *      Author: antonis
- */
-
 #include <gecode/int.hh>
 #include <gecode/minimodel.hh>
 #include <gecode/search.hh>
 
 #include "A1.cpp"
+
+#define N 9
 
 using namespace Gecode;
 
@@ -17,41 +12,31 @@ class Sudoku : public Space {
 protected:
 	IntVarArray cells;
 public:
-	Sudoku(int raw[9 * 9]) : cells(*this, 9*9, 1, 9) {
-
-		/*for (int i = 0; i < 9 * 9; i++) {
-			std::cout << raw[i];
-			if ((i % 9) == 0)
-				std::cout << std::endl;
-		}*/
+	Sudoku(int raw[N * N]) : cells(*this, N * N, 1, N) {
 		// Initialize cells
-		Matrix<IntVarArray> matrix(cells, 9, 9);
+		Matrix<IntVarArray> matrix(cells, N, N);
 
 		// Post constraints
-
 		// Fixed numbers
-		for (int i = 0; i < 9*9 ; i++) {
+		for (int i = 0; i < N * N ; i++) {
 			if (raw[i] != 0) {
-				int row = i / 9;
-				int column = i % 9;
-				std::cout << "Row: " << row << " Col: " << column << " Val: " << raw[i] << std::endl;
+				int row = i / N;
+				int column = i % N;
+				//std::cout << "Row: " << row << " Col: " << column << " Val: " << raw[i] << std::endl;
 				rel(*this, matrix(column, row), IRT_EQ, raw[i]);
 			}
 		}
-		Slice<IntVarArray> lala = matrix.row(2);
-		std::cout << lala << std::endl;
 
 		// Distinct row
-		for (int i = 0; i < 9; i++)
+		for (int i = 0; i < N; i++)
 			distinct(*this, matrix.row(i));
 		// Distinct column
-		for (int i = 0; i < 9; i++)
+		for (int i = 0; i < N; i++)
 			distinct(*this, matrix.col(i));
 		// Distinct 3x3 block
-		for (int i = 0; i < 9; i += 3)
-			for (int j = 0; j < 9; j += 3)
+		for (int i = 0; i < N; i += 3)
+			for (int j = 0; j < N; j += 3)
 				distinct(*this, matrix.slice(i, i + 3, j, j + 3));
-
 
 		// Branching
 		branch(*this, cells, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
@@ -74,15 +59,15 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-	int raw[9 * 9];
+	int raw[N * N];
 	int counter = 0;
 
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			int index = counter + j;
 			raw[index] = examples[0][i][j];
 		}
-		counter += 9;
+		counter += N;
 	}
 	// Create model
 	Sudoku* sudoku = new Sudoku(raw);
